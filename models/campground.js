8a00a2c53d95647f5c489a4099b4924f6465ad50
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2020-12-30 14:54:36 -0800

    Yelp Camp
    Added node modules

--- models/campground.js
+++ models/campground.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Schema ({
+const CampgroundSchema = new Schema({
     title: String,
     price: String,
     description: String,
--- seeds/index.js
+++ seeds/index.js
@@ -10,22 +10,24 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 });
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
 
-const sample = array => {array[Math.floor(Math.random() * array.length)]};
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`
         })
-        await camp.save()
+        await camp.save();
     }
 }
 
