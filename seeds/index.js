b66594881a97a6c95ac4ba5ece733a13163aa3cc
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-01-01 11:49:38 -0800

    Yelp Camp
    Styled pages

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam magnam voluptatem, ipsum voluptas nesciunt earum est commodi a quibusdam voluptates hic tempora temporibus repudiandae, veniam amet laudantium id repellendus distinctio!',
+            price:
         })
         await camp.save();
     }
