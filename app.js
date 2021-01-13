b667db30bbcab66ae57584f3fed3669ac09d1f0e
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-01-13 08:49:30 -0800

    YelpCamp
    Register form

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user')
 
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -65,9 +66,10 @@ app.use((req, res, next) => {
 })
 
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
-app.use(express.static('public'))
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
 
 app.get('/', (req, res) => {
     res.render('home')
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
-    res.render()
+    res.render('users/register');
+});
+
+router.post('/register', async(req, res) => {
+    const {email, username, password} = req.body;
+    const user = new User({email, username});
+    const registeredUser = await User.register(user, password);
 })
 
 module.exports = router;
