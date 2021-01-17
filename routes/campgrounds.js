7e4cf345d2868eb16e29d95d069e0e27e1970ea0
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-01-17 10:03:02 -0800

    YelpCamp
    Displayed star ratings

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const campgrounds = require('../controllers/campgrounds')
+const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
@@ -13,10 +13,12 @@ router.route('/')
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
-    .get(catchAsync(campgrounds.ShowCampground))
+    .get(catchAsync(campgrounds.showCampground))
     .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
+
+
 
 module.exports = router;
